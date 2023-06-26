80e56206bba6d40b402b05da512bdb9bdc38076c
math2w <73835298+math2w@users.noreply.github.com>
2023-06-26 20:14:30 +0200

    Registering system
    You can now register. The users data is stored at firebase database.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
   import { getAuth} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-  import { getDatabase} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js"; 
+  import { getDatabase, ref, set, get, child} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js"; 
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,69 +23,53 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
-const auth = getAuth();
+const auth = getAuth(app);
 const database = getDatabase();
 
 //register function
 
 const signupbtn = document.getElementById("signup");
-
 function register() {
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
+    const username = document.getElementById("username").value;
+    const usernamestyle = document.getElementById("username").style;
+    const password = document.getElementById("password").value;
+    const alreadyuse = document.getElementById("alreadyuse");
+    const usernames = ref(database, 'users/' + username + '/username');
 
-
-    if (validateEmail(email == false) || validateField(email || password)) {
-        return;
-    }
-
-    auth.createUserWithEmailAndPassword(email, password);
-    then(function() {
-        var user = auth.currentUser;
-        alert("User registered");
-
-        var databaseref = database.ref;
-
-        var userdata = {
-            email : email,
-            last_login : Date.now()
+    if (validateField(username, password) == false) {
+      return;  
+    };
+    const dbref = ref(getDatabase());
+    get(child(dbref, "users/" + username)).then((snapshot) => {
+        if (snapshot.exists()) {
+            //alert("Username already exists!");
+            usernamestyle.borderColor = "red";
+            alreadyuse.hidden = false
+            return;
+        } else {
+            set(ref(database, 'users/' + username), {
+                username: username,
+                password: password
+                
+            });
+            window.location.replace('login.html');
         }
-
-        databaseref.child('users/' + user.uid).set(userdata);
-    })
-    .catch(function(error) {
-        var error_code = error.code;
-        var error_message = error.message;
-
-        alert(error_message);
-    })
+    });
 }
+function validateField(username, password) {
 
-function validateEmail(email) {
-    const val = /^[^@]+@\w+(\.\w+)+\w$/;
-
-    if (val.test(email) == true) {
-        return true;
-
-    } else {
-
-        return false;
-    }
-}
-
-function validateField(field) {
-    if (field == null) {
-        return false;
-    }
-
-    if (field.length <= 0) {
+    if (username.length <= 0 || password.length <= 0) {
+        alert('type something!');
         return false;
     } else {
         return true;
     }
 }
+
+
 
 signupbtn.onclick = function() {
+    console.log('attempting to sign up');
     register();
-    alert('done!');
-}
+
+};
